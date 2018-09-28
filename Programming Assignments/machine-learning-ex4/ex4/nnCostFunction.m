f5073a2b1be78f492fbe7bb0d89782b7d1b0feed
Kian Boon Koh <kianboonkoh@gmail.com>
2018-09-28 14:47:36 +0800

    add regularization term for gradient in ex4

--- Programming Assignments/machine-learning-ex4/ex4/nnCostFunction.m
+++ Programming Assignments/machine-learning-ex4/ex4/nnCostFunction.m
@@ -110,23 +110,10 @@ Theta2_grad = delta2 ./ m;
 %               and Theta2_grad from Part 2.
 %
 
+% Add regularization term for gradient
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + (lambda/m) .* Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + (lambda/m) .* Theta2(:, 2:end);
 
 % -------------------------------------------------------------
 
